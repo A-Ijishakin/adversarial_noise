25133d4b2fcb7bfe9b81e87c614318a16cfdccd3
A-ijishakin <ayodeji.ijishakin.21@ucl.ac.uk>
2024-01-26 16:35:58 +0000

    Can Perform an Adversarial Attack

--- adversary.py
+++ adversary.py
@@ -1,6 +1,10 @@
 import torch 
 from PIL import Image
 from torchvision.models import resnet50, ResNet50_Weights  
+from torch.utils.tensorboard import SummaryWriter 
+import torch.nn as nn
+from torch.nn import functional as F 
+
 
 class Adversary:
     """ 
@@ -26,6 +30,16 @@ class Adversary:
         
         #set up the pre-processin pipeline 
         self.pre_process = self.weights.transforms()  
+        
+        #initialise the loss function
+        self.loss = nn.CrossEntropyLoss() 
+        
+        #initialise the logger the runs folder is in the gitignore to save space 
+        self.logger = SummaryWriter('runs') 
+        
+        #initialise the noise tensor
+        self.noise = torch.zeros(3, 224, 224).to(self.device)
+        
         
           
     def classify(self, img_path: str):
@@ -60,10 +74,84 @@ class Adversary:
         class_name = self.weights.meta["categories"][predicted_class]
 
         #print the results 
-        print(f"Predicted class: {class_name} (With Confidence of {100 * likelihood})") 
+        print(f"Predicted class: {class_name} (With Confidence of {100 * likelihood})")  
         
+        
+        
+    def generate_adversarial_noise(self, img_path : str, actual_index: int, target_index: int,
+                                   lr=0.05, steps=500):
+        
+        """ 
+        generate_adversarial_noise:
+            Generates adversarial noise for an image.
+            
+        Args:
+            img_path (str): path to the image file
+            actual_index (int): the index of the actual class
+            target_index (int): the index of the target class
+            lr (float): the learning rate of the optimizer
+            steps (int): the number of steps to take in the direction of the gradient 
+            
+        Returns:
+            torch.Tensor: the adversarial noise tensor
+            
+        """
+        
+        #set the noise tensor to be a parameter that requires a gradient 
+        noise = self.noise.detach().requires_grad_() 
+        
+        #set up the optimizer
+        optimizer = torch.optim.Adam([noise], lr=lr) 
+        
+        #get the one hot encoded vector for the actual class 
+        actual_class = F.one_hot(torch.tensor(actual_index), num_classes=1000).to(torch.float32).to(self.device)
+        
+        #get the one hot encoded vector the target class 
+        adversarial_class = F.one_hot(torch.tensor(target_index), num_classes=1000).to(torch.float32).to(self.device)
+        
+        #preprocess the image 
+        img = self.pre_process(Image.open(img_path)).to(self.device) 
+        
+        #iterate over the number of steps
+        for iteration in range(steps): 
+            
+            #compute the adversarial image 
+            adversarial_img = img + noise 
+            
+            #make a classification of the adversarial image 
+            class_pred = self.model(adversarial_img.unsqueeze(0)).squeeze(0).softmax(0) 
+            
+            #compute the negative of the loss for the actual class by computing the negative we ensure the total loss will minimise the likelihood of this class 
+            original_loss = - self.loss(class_pred, actual_class) 
+            
+            #compute the loss for the adversarial class this will maximise the likelihood of the adversarial class
+            adversarial_loss = self.loss(class_pred, adversarial_class)
+            
+            #compute the total loss 
+            loss = original_loss + adversarial_loss 
+            
+            #zero the gradients
+            optimizer.zero_grad()
+            #backpropagate the loss
+            loss.backward() 
+            #take a step in the direction of the gradient 
+            optimizer.step() 
+            
+            #log the loss and the predicted class 
+            self.logger.add_scalar('Loss', loss.item(), iteration) 
+            self.logger.add_text('Predicted Class', self.weights.meta["categories"][class_pred.argmax().item()], iteration) 
+            
+            #check if the adversarial class has been found
+            if class_pred.argmax().item() == target_index:
+                #if it has print the number of steps taken and break out of the loop
+                print(f"Adversarial Noise Found in {iteration} steps")
+                break
+            
+        return noise.detach() 
 
-#some example images have been downloaded from kaggle at: https://www.kaggle.com/datasets/ifigotin/imagenetmini-1000 this is one 
-test_img = 'images/n01695060_1676.jpg' 
+#some example images have been downloaded from kaggle at: https://www.kaggle.com/datasets/ifigotin/imagenetmini-1000 this is one: renamed to giant_panda.jpg
+test_img = 'images/giant_panda.jpg' 
 
-Adversary().classify(test_img)
+if __name__ == "__main__":
+    #test the generate_adversarial_noise function here the target class is a goldfish 
+    Adversary().generate_adversarial_noise(test_img, 388, 1) 
